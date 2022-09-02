f56bc0ccea59e9d1b428807a5cb532ea423c70d5
jhonpulido1990 <3825@holbertonstudents.com>
2022-09-02 12:05:58 -0500

    Closure

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,29 @@
+/*
+Write a function named welcomeMessage:
+
+    It accepts one argument fullName (string)
+    It should be a closure for an alert displaying Welcome <fullName>
+
+After this function definition, create three variables:
+
+    guillaume contains a call welcomeMessage with Guillaume as argument
+    alex contains a call welcomeMessage with Alex as argument
+    fred contains a call welcomeMessage with Fred as argument
+*/
+function welcomeMessage(fullName){
+    return () => (alert('Welcome' + " " + `${fullName}`))
+}
+
+let guillaume = welcomeMessage("Guillaume");
+let alex = welcomeMessage("Alex");
+let fred = welcomeMessage("Fred");
+
+/*
+How to test:
+
+    In your web console, cop/paste your file
+    Executing the following in the console:
+        guillaume();
+        alex();
+        fred();
+*/
